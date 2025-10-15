cbf4813c0980b1765b12e951019a6db7b4bbd371
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-10-15 16:05:41 -0300

    Se agrego la estructura de carpetas y archivos

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,23 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      
+
+    </div>
+  );
+}
+
+
+/**
+ * 
+ *  boton de request scraper: dominios, listado de scraper
+ *  detalle del scraper: scraperapi
+ * over view: los graficos
+ *            los creditos? como mostrarlos?
+ * 
+ * 
+ */
