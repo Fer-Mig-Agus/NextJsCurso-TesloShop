080f194a8c2cb0d3d26d65aebd88543be9fe2951
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-10-15 17:03:33 -0300

    Se agrego el top menu

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hola mundo</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
+
 
     </div>
   );
@@ -14,10 +13,10 @@ export default function Home() {
 
 /**
  * 
- *  boton de request scraper: dominios, listado de scraper
+ *  boton de request scraper: dominios, listado de scraper 
  *  detalle del scraper: scraperapi
  * over view: los graficos
  *            los creditos? como mostrarlos?
  * 
  * 
- */
+ */ 
