fade88e3f097b9dd211aa094ee2da77b5737dd6f
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-10-16 17:08:18 -0300

    Se agrego la pantalla de buscar por categoria, falta mejorar para que controle el error de si busca otra categoria por url

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,24 +1,56 @@
-import { notFound } from "next/navigation";
+"use client";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import { notFound, usePathname } from "next/navigation";
+
+// interface Props {
+//   params: {
+//     id: string;
+//   };
+// }
 
 
-
-interface Props{
-  params:{
-    id:string;
-  }
+interface Props {
+  params: {
+    id: 'men' | 'women' | 'kid';
+  };
 }
 
-const Category = ({params}:Props) => {
 
-  if (params.id === 'kids') {
-    notFound();
+const Category = ({ params }: Props) => {
+
+  // if (params.id) {
+  //   notFound();
+  // }
+
+  // const pathName = usePathname();
+
+  // const category=pathName.split('/').pop()
+
+  const {id}=params
+
+  const data = initialData.products.filter((item) => item.gender === id);
+
+  const labels = {
+    'men':'Men',
+    'women':'Women',
+    'kid':'Kids'
   }
+
+  
 
   return (
     <div>
-      <h1>Category page</h1>
+      
+            <Title
+            title={`${labels[id]}'s section   `}
+            subtitle="All products"
+            className="mb-2"
+            />
+      <ProductGrid products={data} />
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
