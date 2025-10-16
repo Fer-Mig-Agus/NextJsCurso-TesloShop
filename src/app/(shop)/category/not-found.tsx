59fcbc05994bdb2ca55f1f130f7bc752af14d211
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-10-16 10:01:49 -0300

    Se realizo la personalizacion del page not found, por el momento solo en category

--- src/app/(shop)/category/not-found.tsx
+++ src/app/(shop)/category/not-found.tsx
@@ -1,12 +1,9 @@
-import Link from "next/link";
+import { PageNotFound } from "@/components";
 import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <Link href={"/"}>Go Back</Link>
-    </div>
+    <PageNotFound/>
   );
 };
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { TopMenu } from "./ui/top-menu/TopMenu";
+export { PageNotFound } from "./ui/not-found/PageNotFound";
+
+
